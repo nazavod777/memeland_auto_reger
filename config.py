8967d10ef44fc8cd73c44c9261334b33c4386a11
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2023-10-20 10:47:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-ref_codes: list = [3222,
-                   6031,
-                   2154]
-
 SITE_KEY: str = '0152B4EB-D2DC-460A-89A1-629838B529C9'
 SITE_URL: str = 'https://twitter.com/account/access'
 FIRSTCAPTCHA_API_KEY: str = ''

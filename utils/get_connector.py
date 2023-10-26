ca85758c51e5a1c9a5883be1d01369db4ec1ca2f
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2023-10-26 18:53:29 +0300

    Update get_connector.py

--- utils/get_connector.py
+++ utils/get_connector.py
@@ -7,6 +7,9 @@ from utils import logger
 
 async def get_connector(proxy: str | None) -> TCPConnector | ProxyConnector:
     try:
+        if proxy and proxy.startswith('https://'):
+            proxy: str = proxy.replace('https://', 'http://')
+
         connector: ProxyConnector | None = ProxyConnector.from_url(url=Proxy.from_str(proxy=proxy).as_url,
                                                                    verify_ssl=False) if proxy else None
         return connector

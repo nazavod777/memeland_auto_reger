4acaf82e513f47286ae2023716cf216b99ff1011
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2023-10-28 13:09:54 +0300

    Update format_range.py

--- utils/format_range.py
+++ utils/format_range.py
@@ -5,14 +5,29 @@ from random import randint
 def format_range(value: str | int,
                  return_randint: bool = False) -> tuple[int, int] | int:
     if str(value).isdigit():
-        return randint(a=int(value),
-                       b=int(value)) if return_randint else int(value), int(value)
+        if return_randint:
+            result: int = randint(a=int(value),
+                                  b=int(value))
+
+        else:
+            result: tuple = int(value), int(value)
 
     elif re.match(pattern=r'^\d+-\d+$',
                   string=value):
         target_digits: list = list(map(int, value.split('-')))
 
-        return randint(a=min(target_digits),
-                       b=max(target_digits)) if return_randint else min(target_digits), max(target_digits)
+        if return_randint:
+            result: int = randint(a=min(target_digits),
+                                  b=max(target_digits))
 
-    return 0, 0
+        else:
+            result: tuple = min(target_digits), max(target_digits)
+
+    else:
+        if return_randint:
+            result: int = 0
+
+        else:
+            result: tuple = 0, 0
+
+    return result
